2405b2f51cd7a01f3b18c79e0709864caf1dc3a3
isactes <fidel.renovato@gmail.com>
2026-07-13 19:29:17 -0600

    jest assertions & matches

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,7 +1,15 @@
 import { toUpperCase } from "../app/Utils";
 describe('Utils', () => {
   test('Should return uppercase', () => {
-    const results = toUpperCase('test');
-    expect(results).toBe('TEST');
+    // arrange
+    const sut  = toUpperCase;
+    const expected = "TEST";
+    //ACT 
+    const  actual = sut("test");
+    //ASSERT
+    expect(actual).toBe(expected);
+
+    //const results = toUpperCase('test');
+    //expect(results).toBe('TEST');
   })
 })
